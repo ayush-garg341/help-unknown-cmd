5f778a8e123bcc0c3afbc718ee35bfcd4c4b41ea
Ayush Garg <gargayush341@gmail.com>
2023-03-16 17:03:56 +0530

    complete code in c along with test cases

--- help_unknown_cmd/help.c
+++ help_unknown_cmd/help.c
@@ -10,6 +10,8 @@
 #define RUN_SETUP (1<<0)
 #define RUN_SETUP_GENTLY (1<<1)
 #define USE_PAGER (1<<2)
+
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 /*
  * require working tree to be present -- anything uses this needs
  * RUN_SETUP for reading from the configuration file.
@@ -28,20 +30,22 @@ struct cmdstruct {
 
 static struct cmdstruct commands[] = {
 	{ "add", RUN_SETUP | NEED_WORK_TREE },
+	{ "push", RUN_SETUP },
+	{ "merge", RUN_SETUP | NEED_WORK_TREE },
 	{ "blame",  RUN_SETUP },
 	{ "cherry-pick", RUN_SETUP | NEED_WORK_TREE },
 	{ "clone" },
+	{ "fetch", RUN_SETUP },
 	{ "commit", RUN_SETUP | NEED_WORK_TREE },
 	{ "diff", NO_PARSEOPT },
-	{ "fetch", RUN_SETUP },
+	{ "revert", RUN_SETUP | NEED_WORK_TREE  },
 	{ "init" },
-	{ "merge", RUN_SETUP | NEED_WORK_TREE },
+	{ "status", RUN_SETUP | NEED_WORK_TREE },
 	{ "pull", RUN_SETUP | NEED_WORK_TREE },
-	{ "push", RUN_SETUP },
+	{ "stash", RUN_SETUP | NEED_WORK_TREE },
+	{ "stage", RUN_SETUP | NEED_WORK_TREE },
 	{ "rebase",  RUN_SETUP | NEED_WORK_TREE },
 	{ "remote", RUN_SETUP },
-	{ "revert", RUN_SETUP | NEED_WORK_TREE  },
-	{ "stash", RUN_SETUP | NEED_WORK_TREE },
 	{ "status", RUN_SETUP | NEED_WORK_TREE }
 };
 
@@ -54,20 +58,99 @@ int main(int argc, char *argv[])
 	const char *input_cmd = argv[1];
 	printf("Input git command is : %s \n", input_cmd);
 	
-	const char *help = help_unknown_cmd(input_cmd);
-	printf("%s\n", help);
+  help_unknown_cmd(input_cmd);
 	return 0;
 }
 
-const char *help_unknown_cmd(const char *cmd)
+static int cmdname_compare(const void *a_, const void *b_)
+{
+	struct cmdname *a = *(struct cmdname **)a_;
+	struct cmdname *b = *(struct cmdname **)b_;
+	return strcmp(a->name, b->name);
+}
+
+static int int_compare(const void *a, const void *b)
+{
+	int *a_int = (int*)a;
+	int *b_int = (int*)b;
+	printf("%d, %d\n", *a_int, *b_int);
+	return *a_int >= *b_int;
+}
+
+static int levenshtein_compare(const void *a_, const void *b_)
+{
+	struct cmdname *a = *(struct cmdname **)a_;
+	struct cmdname *b = *(struct cmdname **)b_;
+
+	int l1 = a->len;
+	int l2 = b->len;
+	return l1 != l2 ? l1 - l2 : strcmp(a->name, b->name);
+}
+
+void help_unknown_cmd(const char *cmd)
 {
 	int i, n, best_similarity = 0;
 	struct cmdnames main_cmds, other_cmds;
 	memset(&main_cmds, 0, sizeof(main_cmds));
 	memset(&other_cmds, 0, sizeof(other_cmds));
 	load_cmd_list("git-", &main_cmds, &other_cmds);
+	QSORT(main_cmds.names, main_cmds.cnt, cmdname_compare);
 
-	return "Implementation going on";
+	printf("Before uniq and after sort\n");
+	for(int i = 0; i < main_cmds.cnt; i++){
+		char *name = main_cmds.names[i]->name;
+		printf("%s ", name);
+	}
+	printf("\n");
+	uniq(&main_cmds);
+	printf("After uniq\n");
+	for(int i = 0; i < main_cmds.cnt; i++){
+		char *name = main_cmds.names[i]->name;
+		printf("%s ", name);
+	}
+	printf("\n");
+	/*int nums[6] = {4, 2, 1, 9, 3, 0};*/
+	/*int **b = &nums;*/
+	/*QSORT(b, 6, int_compare);*/
+	/*for(int i = 0; i < 6; i++){*/
+		/*printf("%d ", nums[i]);*/
+	/*}*/
+
+	for (i = 0, n = 0; i < main_cmds.cnt; i++){
+		const char *candidate = main_cmds.names[i]->name;
+		main_cmds.names[i]->len = levenshtein(cmd, candidate);
+	}
+	QSORT(main_cmds.names, main_cmds.cnt, levenshtein_compare);
+	printf("Sorted based on levenshtein distance\n");
+	for(int i = 0; i < main_cmds.cnt; i++){
+		char *name = main_cmds.names[i]->name;
+		printf("%s ", name);
+	}
+	printf("\n");
+	/* skip and count prefix matches */
+	/* len = 0 when input cmd matches candidate cmd */
+	for (n = 0; n < main_cmds.cnt && !main_cmds.names[n]->len; n++)
+		;
+	printf("n = %d\n", n);
+
+	/* count all the most similar ones */
+	for (best_similarity = main_cmds.names[n++]->len; (n < main_cmds.cnt && best_similarity == main_cmds.names[n]->len); n++)
+		;
+
+	fprintf(stderr, ("git: '%s' is not a git command. See 'git --help'."), cmd);
+	if (SIMILAR_ENOUGH(best_similarity)) {
+		const char *msg = "\nThe most similar command is";
+		if (n > 1){
+			msg = "\nThe most similar commands are";
+		}
+		fprintf(stderr, (msg), "");
+
+		printf("\n");
+		for (i = 0; i < n; i++)
+			fprintf(stderr, "\t%s\n", main_cmds.names[i]->name);
+	}
+
+	exit(1);
 }
 
 void load_cmd_list(const char *prefix, struct cmdnames *main_cmds, struct cmdnames *other_cmds){
@@ -76,6 +159,8 @@ void load_cmd_list(const char *prefix, struct cmdnames *main_cmds, struct cmdnam
 	printf("PATH : %s\n ", envPath);
 	printf("EXEC PATH : %s\n", exec_path);
 	load_builtin_commands(prefix, main_cmds);
+	// other_cmds are loaded from envPaths, skipping this..
+	exclude_cmds(other_cmds, main_cmds);
 }
 
 void load_builtin_commands(const char *prefix, struct cmdnames *main_cmds){
@@ -92,7 +177,6 @@ void load_builtin_commands(const char *prefix, struct cmdnames *main_cmds){
 	printf("After skipping, prefix is %s\n", prefix);
 	for(i = 0; i < ARRAY_SIZE(commands); i++){
 		if(skip_prefix(commands[i].cmd, prefix, &name)){
-			printf("Command is %s\n", name);
 			add_cmdname(main_cmds, name, strlen(name));
 		}
 		
@@ -111,9 +195,85 @@ int skip_prefix(const char *str, const char *prefix, const char **out){
 }
 
 void add_cmdname(struct cmdnames *main_cmds, const char *name, int len){
+	printf("command name is %s and len is %d \n", name, len);
 	struct cmdname *ent;
 	FLEX_ALLOC_MEM(ent, name, name, len);
 	ent->len = len;
+	printf("Main_cmds count: %d and alloc: %d \n", main_cmds->cnt, main_cmds->alloc);
 
 	// Alloc grow on main_cmds
+	ALLOC_GROW(main_cmds->names, main_cmds->cnt + 1, main_cmds->alloc);
+	main_cmds->names[main_cmds->cnt++] = ent;
+}
+
+void uniq(struct cmdnames *cmds)
+{
+	int i;
+	int j = 0;
+	if(!cmds->cnt){
+		return;
+	}
+	for(i = 1; i < cmds->cnt; i++){
+		if(!strcmp(cmds->names[i]->name, cmds->names[j]->name)){
+			free(cmds->names[i]);
+		}
+		else{
+			j += 1;
+			cmds->names[j] = cmds->names[i];
+		}
+	}
+	cmds->cnt = j + 1;
+}
+
+void exclude_cmds(struct cmdnames *cmds, struct cmdnames *excludes)
+{
+	int ci, cj, ei;
+	int cmp;
+
+	ci = cj = ei = 0;
+	while (ci < cmds->cnt && ei < excludes->cnt){
+		cmp = strcmp(cmds->names[ci]->name, excludes->names[ei]->name);
+		if (cmp < 0){
+			cmds->names[cj++] = cmds->names[ci++];
+		}
+		else if(cmp == 0){
+			ei++;
+			free(cmds->names[ci++]);
+		}
+		else {
+			ei++;
+		}
+	}
+
+	while (ci < cmds->cnt){
+		cmds->names[cj++] = cmds->names[ci++];
+	}
+	cmds->cnt = cj;
+}
+
+int levenshtein(const char *string1, const char *string2)
+{
+	// Actual git implementation uses Damerau-Levenshtein (weighted) algo
+	int len1 = strlen(string1);
+	int len2 = strlen(string2);
+	int dp[len1 + 1][len2 + 1];
+	for(int i = 0; i <= len2; i++){
+		dp[0][i] = i;
+	}
+
+	for(int j = 0; j <= len1; j++){
+		dp[j][0] = j;
+	}
+	for(int i = 1; i <= len1; i++){
+		for(int j = 1; j <= len2; j++){
+			if(string1[i-1] == string2[j-1]){
+				dp[i][j] = dp[i-1][j-1];
+			}
+			else{
+				dp[i][j] = 1 + MIN(dp[i-1][j-1], MIN(dp[i][j-1], dp[i-1][j]));
+			}
+		}
+	}
+
+	return dp[len1][len2];
 }
--- help_unknown_cmd/help.h
+++ help_unknown_cmd/help.h
@@ -31,6 +31,72 @@
 } while(0)
 
 /*
+ * Dynamically growing an array.
+ * Define your array with:
+ * - a pointer (`item`) that points at the array, initialized to `NULL`
+ *
+ * - an integer variable (`alloc`) that keeps track of how big the current
+ *   allocation is, initialized to `0`;
+ *
+ * - another integer variable (`nr`) to keep track of how many elements the
+ *   array currently has , initialized to `0`.
+ *
+ * Then before adding `n`th element to the item, call `ALLOC_GROW(item, n, alloc)`
+ * This ensures that the array can hold atleast n elements by calling `realloc` and
+ * adjusting `alloc` variable.
+ *
+ * ------------------------
+ * sometype *item;
+ * size_t nr;
+ * size_t alloc;
+ *
+ * ALLOC_GROW(item, nr+1, alloc);
+ * item[nr++] = value you like
+ *
+ */
+
+/*
+ * Realloc deallocates the old object pointed to by ptr and returns a pointer to a new object that has the size specified by size.
+ * The contents of the new object is identical to that of the old object prior to deallocation, up to the lesser of the new and old sizes.
+ * realloc() should only be used for dynamically allocated memory.
+ *
+ *    void *realloc(void *ptr, size_t size);
+ *
+ *      int *ptr = (int *)malloc(sizeof(int)*2);
+ *      int i;
+ *      int *ptr_new;
+ *      *ptr = 10;
+ *      *(ptr + 1) = 20;
+ *      ptr_new = (int *)realloc(ptr, sizeof(int)*3);
+ *      *(ptr_new + 2) = 30;
+ *      for(i = 0; i < 3; i++)
+ *          printf("%d ", *(ptr_new + i)); // 10, 20, 30
+ *
+ */
+
+static inline size_t st_mult(size_t a, size_t b) {
+	return a*b;
+}
+
+
+#define REALLOC_ARRAY(x, alloc) (x) = realloc((x), st_mult(sizeof(*(x)), (alloc)))
+
+#define alloc_nr(x) (((x)+16)*3/2)
+
+#define ALLOC_GROW(x, nr, alloc)\
+	printf("%d, %d\n", nr, alloc); \
+	do { \
+		if ((nr) > alloc){\
+			if(alloc_nr(alloc) < (nr)) \
+				alloc = (nr); \
+			else \
+				alloc = alloc_nr(alloc); \
+			REALLOC_ARRAY(x, alloc); \
+		}\
+	} while(0)
+
+
+/*
  * void *xmalloc(size_t size);
  * void *xcalloc(size_t nmemb, size_t size);
  * void *xrealloc(void *ptr, size_t size);
@@ -73,10 +139,26 @@ struct cmdnames {
 	} **names;
 };
 
-const char *help_unknown_cmd(const char *cmd);
+void help_unknown_cmd(const char *cmd);
 void load_cmd_list(const char *prefix, struct cmdnames *main_cmds, struct cmdnames *other_cmds);
 void load_builtin_commands(const char *prefix, struct cmdnames *main_cmds);
 int skip_prefix(const char *str, const char *prefix, const char **out);
 void add_cmdname(struct cmdnames *main_cmds, const char *name, int len);
+void uniq(struct cmdnames *cmds);
+void exclude_cmds(struct cmdnames *cmds, struct cmdnames *excludes);
+int levenshtein(const char *string1, const char *string2);
+
+void git_stable_qsort(void *base, size_t nmemb, size_t size, int *compare(const void *, const void *));
+
+#define qsort git_stable_qsort
+
+static inline void sane_qsort(void *base, size_t nmemb, size_t size,
+									int *compare(const void *, const void *))
+{
+	if(nmemb > 1){
+		qsort(base, nmemb, size, compare);
+	}
+}
+#define QSORT(base, n, compare) sane_qsort((base), (n), sizeof(*base), compare)
 
 #endif /* HELP_H */
